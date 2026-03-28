9bc1f605ac694802e32651539bd4fbc79e736321
Jakub Bujak <jakbuj3@gmail.com>
2026-03-28 11:26:05 +0100

    fixed flashing on only visible sprite hitbox

--- src/core/utils/Flash.py
+++ src/core/utils/Flash.py
@@ -27,9 +27,12 @@ class Flash:
             return image
         
         flash_alpha = int(self.max_alpha * (1 - self.timer / self.duration))
-        
-        flash_surface = pygame.Surface(image.get_size(), pygame.SRCALPHA)
-        flash_surface.fill((*self.color, flash_alpha))
+
+        mask = pygame.mask.from_surface(image, threshold=1)
+        flash_surface = mask.to_surface(
+            setcolor=(*self.color, flash_alpha),
+            unsetcolor=(0, 0, 0, 0),
+        )
         
         flashed_image = image.copy()
         flashed_image.blit(flash_surface, (0, 0), special_flags=pygame.BLEND_RGBA_ADD)
